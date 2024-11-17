18139a1c56cfc2d7c23156c7de1d7de3fbf026d0
leif <leif@elliottnet.com>
2024-11-17 20:33:46 +0800

    added information

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,3 +2,5 @@
 	<title>Contact Professional Web3 Developers</title>
 	<meta name="description" content="" />
 </svelte:head>
+
+<div class="prose">Contact information goes here</div>
--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -2,3 +2,7 @@
 	<title>Learn Web3 Development on Cardano</title>
 	<meta name="description" content="" />
 </svelte:head>
+
+<div class="prose">
+	<h1>Learning Materials Go Here</h1>
+</div>
