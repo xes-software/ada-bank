20566fbfce550a23e5c6e9f95d02e65c55946dcb
leif <leif@elliottnet.com>
2024-11-17 14:13:38 +0800

    Adding some content for the insight page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <svelte:head>
-	<title>xes.software - Building & Educating on Cardano</title>
-	<meta name="description" content="" />
+	<title>xes.software</title>
+	<meta name="description" content="Building & Educating in the Web3 Cardano Ecosystem" />
 </svelte:head>
 
 <div class="prose">
-	<h1 class="text-primary">Welcome to xes.software</h1>
+	<h1 class="text-primary">
+		Built with Aiken, MeshJS, Sveltekit, shadcn-svelte, vercel, and cloudflare!
+	</h1>
 </div>
--- src/routes/insights/+page.svelte
+++ src/routes/insights/+page.svelte
@@ -2,3 +2,7 @@
 	<title>Insights into Professional Web3 Development</title>
 	<meta name="description" content="" />
 </svelte:head>
+
+<div class="prose">
+	<h1>Content for the insight page</h1>
+</div>
